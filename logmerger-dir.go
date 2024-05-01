3545a69e17811ea05450c612936135b35bb36cf8
rosbit <me@rosbit.cn>
2024-05-01 19:29:03 +0800

    handler count argument support

--- logmerger-dir.go
+++ logmerger-dir.go
@@ -13,19 +13,18 @@ import (
 type chanHandler struct {
 	logFile string
 	fileHandler FnFileHandler
-	dontMerge bool
 }
 
 /**
  * The main process: monitor rootDir, handle files with ext with fileHandler, and merge them to daily files.
  * The process will loop forever until Stop() called.
  */
-func (lm *LogMerger) RunDir(rootDir string, ext string, fileHandler FnFileHandler) {
-	lm.runDir(rootDir, ext, fileHandler, false)
+func (lm *LogMerger) RunDir(rootDir string, ext string, fileHandler FnFileHandler, maxHandlerCount ...int) {
+	lm.runDir(rootDir, ext, fileHandler, maxHandlerCount...)
 }
 
-func (lm *LogMerger) runDir(rootDir string, ext string, fileHandler FnFileHandler, dontMerge bool) {
-	ch := lm.startDirFileHandlers()
+func (lm *LogMerger) runDir(rootDir string, ext string, fileHandler FnFileHandler, maxHandlerCount ...int) {
+	ch := lm.startDirFileHandlers(maxHandlerCount...)
 
 	for !lm.exit {
 		fileSystem := os.DirFS(rootDir)
@@ -39,7 +38,6 @@ func (lm *LogMerger) runDir(rootDir string, ext string, fileHandler FnFileHandle
 			ch <- &chanHandler{
 				logFile: p.Join(rootDir, path),
 				fileHandler: fileHandler,
-				dontMerge: dontMerge,
 			}
 			return nil
 		})
@@ -49,21 +47,24 @@ func (lm *LogMerger) runDir(rootDir string, ext string, fileHandler FnFileHandle
 	log.Printf("[logmerger] I will exit\n")
 }
 
-func (lm *LogMerger) startDirFileHandlers() (chan *chanHandler)  {
-	const maxHandlers = 3
+func (lm *LogMerger) startDirFileHandlers(maxHandlerCount ...int) (chan *chanHandler)  {
+	maxHandlers := 3
+	if len(maxHandlerCount) > 0 && maxHandlerCount[0] > 0 {
+		maxHandlers = maxHandlerCount[0]
+	}
 
 	ch := make(chan *chanHandler, maxHandlers)
 	for i:=0; i<maxHandlers; i++ {
 		go func(lm *LogMerger, i int, ch <-chan *chanHandler) {
 			for h := range ch {
-				lm.runPatternFile(h.logFile, h.fileHandler, h.dontMerge)
+				lm.runPatternFile(h.logFile, h.fileHandler)
 			}
 		}(lm, i, ch)
 	}
 	return ch
 }
 
-func (lm *LogMerger) runPatternFile(logFile string, fileHandler FnFileHandler, dontMerge bool) {
+func (lm *LogMerger) runPatternFile(logFile string, fileHandler FnFileHandler) {
 	reuseLogFile := fmt.Sprintf("%s%s", logFile, _REUSE_SUFFIX)
 	var lf string
 	if _, err := os.Stat(reuseLogFile); err == nil {
@@ -73,6 +74,6 @@ func (lm *LogMerger) runPatternFile(logFile string, fileHandler FnFileHandler, d
 	} else {
 		return
 	}
-	lm.processLogFile(lf, fileHandler, dontMerge)
+	lm.processLogFile(lf, fileHandler, false)
 }
 
